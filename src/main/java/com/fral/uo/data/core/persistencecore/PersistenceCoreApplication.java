7e4815fb261371dacbdbae3c42895018913767a9
franco-extreme <franck_ral@hotmail.com>
2018-04-28 00:45:50 -0400

    Starting initial structure of the project.

---
+++ src/main/java/com/fral/uo/data/core/persistencecore/PersistenceCoreApplication.java
@@ -0,0 +1,12 @@
+package com.fral.uo.data.core.persistencecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceCoreApplication.class, args);
+	}
+}
