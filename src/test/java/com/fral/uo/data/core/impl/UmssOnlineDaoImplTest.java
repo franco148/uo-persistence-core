f2dcd8b7929f5ec55fb64791c0eca033fc73a46d
franco-extreme <franck_ral@hotmail.com>
2018-04-29 21:31:08 -0400

    UnitTestsImplementation: Added DaoImplementationTest file and added required configuration in application.properties file.

---
+++ src/test/java/com/fral/uo/data/core/impl/UmssOnlineDaoImplTest.java
@@ -0,0 +1,28 @@
+package com.fral.uo.data.core.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UmssOnlineDaoImplTest {
+
+    @Test
+    public void persist() {
+    }
+
+    @Test
+    public void persist1() {
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void find1() {
+    }
+}
